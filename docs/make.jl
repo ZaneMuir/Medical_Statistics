4b4a3a6e31f81c50742fde77655f051ec1731a87
Zane Muir <zanemuir1995@gmail.com>
2018-04-07 17:41:29 +0800

    final trial

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,4 @@ makedocs()
 
 deploydocs(
   repo="github.com/ZaneMuir/Medical_Statistics.git",
-  deps=Deps.pip("mkdocs", "python-markdown-math"),
-  osname="osx"
-)
+  deps=Deps.pip("mkdocs", "python-markdown-math"))
